e6244d32110a2ac5ac4be770b9dac39e2015609b
HeartBreakerrr <rafafabian18@gmail.com>
2021-10-11 05:16:57 -0500

    Orden number and sum of a set of numbers

--- Lowest to highest Highest to lowest sum and number of digits/Order and sum numbers program.py
+++ Lowest to highest Highest to lowest sum and number of digits/Order and sum numbers program.py
@@ -1,27 +1,31 @@
-#Obtenemos datos, eliminamos espacios con .split
-num = input("Escribe los números que ordenar ").split(" ")
+def main():
+ #Obtenemos datos, eliminamos espacios con .split
+ num = input("Escribe los números que ordenar ").split(" ")
 
-#A lista y ordenamos
-print("Números ordenados, menor a mayor")
-num=list(num)
-num.sort()
-print(num)
+ #A lista y ordenamos
+ print("Números ordenados, menor a mayor")
+ num=list(num)
+ num.sort()
+ print(num)
 
-#Ordenamos mayor a menor
-print("")
-print("Mayor a menor")
-num.sort(reverse=True)
-print(num)
+ #Ordenamos mayor a menor
+ print("")
+ print("Mayor a menor")
+ num.sort(reverse=True)
+ print(num)
 
-#Datos en la lista
-print("")
-print("Número de dígitos")
-print(len(num))
+ #Datos en la lista
+ print("")
+ print("Número de dígitos")
+ print(len(num))
 
-#Creamos generador, str a int para operar con él
-#R.I.P RAM
-num = (int(x) for x in num)
-#Operador
-print("")
-print("Suma de los números")
-print(sum(num))
+ #Creamos generador, str a int para operar con él
+ #R.I.P RAM
+ num = (int(x) for x in num)
+ #Operador
+ print("")
+ print("Suma de los números")
+ print(sum(num))
+
+if __name__ == "__main__":
+    main()
